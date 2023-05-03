af1222a80af19236e7e664ac445d24830f9603e3
Eunille <eunille.villegas@growsari.com>
2023-05-03 14:33:47 +0800

    not fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier",
+        "prettier/react"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-native"
+    ],
+    "rules": {
+        "react-native/no-unused-styles": 2,
+        "react-native/split-platform-components": 2,
+        "react-native/no-inline-styles": 2,
+        "react-native/no-color-literals": 2,
+        "react-native/no-raw-text": 2
+    }
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,15 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  // "arrowParens": "always",
+  // "bracketSpacing": true,
+  // "jsxBracketSameLine": false,
+  // "jsxSingleQuote": false,
+  // "quoteProps": "as-needed",
+  // "singleQuote": true,
+  // "semi": true,
+  // "printWidth": 100,
+  // "useTabs": false,
+  // "tabWidth": 2,
+  // "trailingComma": "es5"
 };
